f7edc6d0edc455083724b4cf56c86352698cb302
subinkr <github@subin.kr>
2021-01-20 07:15:55 +0900

    merge 추가

--- lib/detail_post_page.dart
+++ lib/detail_post_page.dart
@@ -105,10 +105,10 @@ class DetailPostPage extends StatelessWidget {
   // 팔로우
   void _follow() {
     Firestore.instance.collection('following').document(user.email)
-        .setData({document['email']: true});
+        .setData({document['email']: true}, merge: true);
 
     Firestore.instance.collection('follower').document(document['email'])
-        .setData({user.email: true});
+        .setData({user.email: true}, merge: true);
   }
 
   // 언팔로우
